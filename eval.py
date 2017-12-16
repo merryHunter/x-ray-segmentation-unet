1b2e639f0719dfd482cf397b1f52695b5046ac79
merryHunter <chernuhaiv@gmail.com>
2017-12-16 14:55:41 +0100

    evaluation metrics added

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ import params
 import matplotlib.pyplot as plt
 import PIL
 import os
+import eval_segm as metric
 
 input_size = params.input_size
 batch_size = params.batch_size
@@ -19,7 +20,7 @@ df_test = None #pd.read_csv('input/sample_submission.csv')
 ids_test = None #df_test['img'].map(lambda s: s.split('.')[0])
 
 OUTPUT_DIR = 'output/'
-
+N_CLASSES = 2
 
 def get_test_mask_paths(filename):
     path_set = []
@@ -77,14 +78,33 @@ def visualize(img_orig, mask_pred, mask_orig):
 
 def get_transparent_prediction(img_orig, mask_pred, alpha=0.5, orig=True):
     output = img_orig.copy()
-    image = np.zeros((orig_height,orig_width, 3), dtype="uint8")
-    image[np.where((mask_pred != [0, 0, 0]).all(axis=2))] = [255, 0, 0]
     if orig:
         image = mask_pred
+    else:
+        image = np.zeros((orig_height, orig_width, 3), dtype="uint8")
+        image[np.where((mask_pred != [0, 0, 0]).all(axis=2))] = [255, 0, 0]
+
     overlay = image.copy()
     cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
     output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
     return output
+
+pixel_accuracy = []
+
+
+def get_pred_orig_grayscale(pred, orig):
+    image_pred = np.zeros((orig_height, orig_width), dtype="uint8")
+    image_pred[np.where((pred != [0, 0, 0]).all(axis=2))] = np.ones((1, 1), dtype="uint8")
+
+    # select indexes where green in intensive so it's a mask
+    mask = orig[:,:,1] > 100
+    image_orig = np.zeros((orig_height, orig_width, 3), dtype="uint8")
+    image_orig[mask] = np.ones((1, 1, 1), dtype="uint8")
+
+    # get back to 2-d array
+    image_orig = image_orig[:,:,0]
+
+    return image_pred, image_orig
 
 
 for start in tqdm(range(0, len(names), batch_size)):
@@ -118,19 +138,32 @@ for start in tqdm(range(0, len(names), batch_size)):
         # visualize(orig_img, 255 * prob, mask_orig)
 
         # get and write transparent mask for predicted image
-        output = get_transparent_prediction(orig_img, p,alpha=0.5,orig=False)
-        cv2.imwrite(cur_dir + str(i) + '_' + ns[i][1].split('/')[-1], output)
+        output_pred = get_transparent_prediction(orig_img, p,alpha=0.5,orig=False)
+        cv2.imwrite(cur_dir + str(i) + '_' + ns[i][1].split('/')[-1], output_pred)
+
         # get and write transparent mask for ground truth
         output = get_transparent_prediction(orig_img, mask_orig,alpha=0.5,orig=True)
-        cv2.imwrite(cur_dir  + 'orig_' + ns[i][1].split('/')[-1], output)
+        cv2.imwrite(cur_dir + 'orig_' + ns[i][1].split('/')[-1], output)
+        pred, orig = get_pred_orig_grayscale(p, mask_orig)
+        cv2.imwrite(cur_dir + 'mask_pred_' + ns[i][1].split('/')[-1],255 * pred)
+        cv2.imwrite(cur_dir + 'mask_orig_' + ns[i][1].split('/')[-1],255 * orig)
+        print pred
+        print orig
+        # plt.imshow(pred)
+        # plt.imshow(orig)
+        # plt.show()
+        iou = metric.mean_accuracy(pred, orig)
+        print "MEAN IU: {0}".format(iou)
 
+        pixel_accuracy.append(iou)
         # for original submission
         mask = prob > threshold
         rle = run_length_encode(mask)
         rles.append(rle)
         i += 1
 
+print "Average pixel accuracy: {0}".format(np.sum(pixel_accuracy) / len(pixel_accuracy))
 
-print("Generating submission file...")
+print("Generating submission file...")  
 df = pd.DataFrame({'img': names, 'rle_mask': rles})
 df.to_csv('submit/submission.csv.gz', index=False, compression='gzip')
